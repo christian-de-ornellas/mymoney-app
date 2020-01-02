861fa455d5ad74995abdfe289c90284957f24733
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-01-02 02:34:07 -0300

    Home ok and refatored

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 import './src/config/StatusBarConfig';
 
 import Home from './src/screens/Home';
 
-class App extends Component {
-  render() {
-    return <Home />;
-  }
-}
+const App = () => <Home />;
 
 export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,32 @@
 import React, {Component} from 'react';
-import {View, Text, Stylesheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 class Home extends Component {
   render() {
-    <View style={styles.container}>
-      <Text style={styles.brand}>My Money</Text>
-      <View style={styles.balance}>
-        <Text style={styles.ballanceText}>Meu Saldo: R$ 200,00</Text>
+    return (
+      <View style={styles.container}>
+        <Text style={styles.brand}>My Money</Text>
+        <View style={styles.balance}>
+          <Text style={styles.ballanceText}>Meu Saldo: R$ 200,00</Text>
+        </View>
       </View>
-    </View>;
+    );
   }
 }
 
-const styles = Stylesheet.create({
+export default Home;
+
+const styles = StyleSheet.create({
   container: {
-    justifyContent: 'space-around',
+    flex: 1,
   },
+
   brand: {
     backgroundColor: '#82ccdd',
     color: '#fff',
     padding: 20,
     fontSize: 20,
   },
-
   balance: {
     alignItems: 'center',
     margin: 10,
@@ -32,5 +36,3 @@ const styles = Stylesheet.create({
     color: '#60a3bc',
   },
 });
-
-export default Home;
