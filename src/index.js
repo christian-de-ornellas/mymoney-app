2e4b4e752cbd2517dbed4ae5f1d36530e2654857
Christian Possidonio <christian.ornellas@calindra.com.br>
2020-01-05 13:49:41 -0300

    Defined color statusBar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+
+import './config/StatusBarConfig';
+
 import Home from './screens/Home';
 import Billing from './screens/Billing';
 
